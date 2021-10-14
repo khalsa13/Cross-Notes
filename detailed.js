a04c8eadf51ddfa04c8a1920b41a3597848161d8
Babnish Vyas <babnishvyas@microsoft.com>
2021-10-14 18:43:57 +0530

    some minor changes

--- detailed.js
+++ detailed.js
@@ -1,13 +1,18 @@
 window.onload = function (event) {
   chrome.runtime.onMessage.addListener(gotMessage);
   function gotMessage(request, sender, sendResponse) {
+    console.log(request);
     if (request.txt === "showDetails") {
       document.getElementById("name").innerText = request.name;
       document.getElementById("description").innerText = request.description;
       document.getElementById("image1").src =
-        "C://Users/ramasingh/Downloads/" + request.image1Url;
+        "C://Users/babnishvyas/Downloads/" + request.image1Url;
+      document.getElementById("anchor1").href =
+          "C://Users/babnishvyas/Downloads/" + request.image1Url;
       document.getElementById("image2").src =
-        "C://Users/ramasingh/Downloads/" + request.image2Url;
+        "C://Users/babnishvyas/Downloads/" + request.image2Url;
+      document.getElementById("anchor2").href =
+          "C://Users/babnishvyas/Downloads/" + request.image2Url;
     }
   }
 };
